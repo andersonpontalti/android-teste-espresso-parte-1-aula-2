83ad2c413a93ad8df56368dabf227747db9ce34b
Anderson Altomare Pontalti <andersonpontalti@NBBV030073.local>
2019-09-20 15:52:54 -0300

    Initial commit

---
+++ app/src/androidTest/java/br/com/alura/leilao/ui/activity/ListaLeilaoTelaTest.java
@@ -0,0 +1,39 @@
+package br.com.alura.leilao.ui.activity;
+
+import android.content.Intent;
+import android.support.test.rule.ActivityTestRule;
+
+import org.junit.Assert;
+import org.junit.Rule;
+import org.junit.Test;
+
+import java.io.IOException;
+
+import br.com.alura.leilao.api.retrofit.client.LeilaoWebClient;
+import br.com.alura.leilao.model.Leilao;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+
+public class ListaLeilaoTelaTest {
+
+    @Rule
+    public ActivityTestRule<ListaLeilaoActivity> activity =
+            new ActivityTestRule<>(ListaLeilaoActivity.class, true, false);
+
+    @Test
+    public void deve_AparecerUmLeilao_QuandoCarregarUmLeilaoNaApi() throws IOException {
+        Leilao carroSalvo = new LeilaoWebClient().salva(new Leilao("Carro"));
+        if(carroSalvo == null){
+            Assert.fail("Leilão não foi salvo");
+        }
+
+        activity.launchActivity(new Intent());
+
+        onView(withText("Carro"))
+                .check(matches(isDisplayed()));
+    }
+
+}
